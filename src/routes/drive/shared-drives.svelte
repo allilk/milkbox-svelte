5d555301ad585de8debc63282885d93c75b28be4
Alli <me@milk.ovh>
2021-01-28 20:16:45 -0500

    Style fixes and shared drive searching

--- src/routes/drive/shared-drives.svelte
+++ src/routes/drive/shared-drives.svelte
@@ -5,6 +5,24 @@
     let keyCode;
     let lineSelected = 0;
     let itemList;
+    const searchGrid = () => {
+        let input, filter, contentList, flex, listitem, i, txtValue;
+        input = document.getElementById("search_input");
+        filter = input.value.toUpperCase();
+        contentList = document.getElementById("content-list");
+        flex = contentList.getElementsByClassName("drive-obj");
+			for (i = 0; i < flex.length; i++) {
+				listitem =flex[i].getElementsByTagName("div")[0];
+					if (listitem) {
+						txtValue = listitem.textContent || listitem.innerText;
+					if (txtValue.toUpperCase().indexOf(filter) > -1) {
+						flex[i].style.display = "";
+					} else {
+						flex[i].style.display = "none";
+					}
+				}       
+			}
+        }
 	const setLoading = async () => {
 		try {
 			let loadingIcon = document.getElementById('#loading');
@@ -191,30 +209,47 @@
 		
 </script>
 <svelte:window on:keydown={handleKeydown}/>
-<div class="top-header shadow-2xl px-8 py-16 sm:py-12 ">
+<div class="top-header shadow-lg px-8 py-16 sm:py-12 ">
     <span id="index-header" class="text-2xl">my shared drives <span id="total-drives" class=" text-gray-500"></span></span>
     <br><hr>
-    quick links: 
-    <a href="drive/root">
-        <button class="font-semibold px-2 py-2 rounded-none shadow">my drive</button>
-    </a>
-    <a href="drive/shared-with-me" >
-        <button class="font-semibold px-2 py-2 rounded-none shadow">shared with me</button>
-    </a>
-    <a href="drive/shared-drives">
-        <button class="font-semibold px-2 py-2 rounded-none shadow">shared drives</button>
-    </a>
+	<div class="mt-2">
+		<span class="text-sm">quick links: </span>
+		<a href="drive/root">
+			<button class="font-semibold px-2 py-2 rounded-none shadow">my drive</button>
+		</a>
+		<a href="drive/shared-with-me" >
+			<button class="font-semibold px-2 py-2 rounded-none shadow">shared with me</button>
+		</a>
+		<a href="drive/shared-drives">
+			<button class="font-semibold px-2 py-2 rounded-none shadow">shared drives</button>
+		</a>
+	</div>
     <br>
 </div>
 <div class="px-4 md:px-8 shadow-inner whitespace-nowrap overflow-x-hidden">
-    <div class="inline-flex space-y-4">
-        <button id="authorize_button" style="display: none;" class="font-semibold px-2 py-2 rounded-none shadow">
-            Authorize</button>
-        <button id="signout_button" style="display: none;" class="font-semibold px-2 py-2 rounded-none shadow">
-            Sign Out</button>
-        <button id="refresh_button" style="display: none;" class="font-semibold px-2 py-2 rounded-none shadow">
-            Refresh</button>
-    </div>
+	<br>
+	<div class="flex items-center">
+		<div class="flex-auto inline-flex">
+			<button id="authorize_button" style="display: none;" class="font-semibold px-2 py-2 rounded-none shadow">
+				Authorize</button>
+			<button id="signout_button" style="display: none;" class="font-semibold px-2 py-2 rounded-none shadow">
+				Sign Out</button>
+			<button id="refresh_button" style="display: none;" class="font-semibold px-2 py-2 rounded-none shadow">
+				Refresh</button>
+		</div>
+		<div class="flex-auto pl-2">
+			<div class="bg-white border relative px-4">
+				<input on:keyup={searchGrid} type="search" name="search" id="search_input" placeholder="Search"
+						class="appearance-none inline-flex w-full outline-none focus:outline-none active:outline-none"/>
+				<button type="submit" class="-ml-3 inline-flex absolute outline-none focus:outline-none active:outline-none bg-transparent hover:bg-transparent">
+					<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						viewBox="0 0 24 24" class="w-6 h-6">
+					<path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+					</svg>
+				</button>
+			</div>
+		</div>
+	</div>
     <div class="grid grid-cols-1 md:grid-cols-2 text-sm sticky px-4 md:px-8">
         <div id="#loading" class="col-span-full">
             <center>
