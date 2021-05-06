e56769a5973ebb0581e13d960491eecc362b7a69
allilk <milk@cream.glass>
2021-05-06 16:24:05 -0400

    Setting up svelte templates for shared drives.

---
+++ src/components/Drive.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let id
+    export let name
+</script>
+
+<div class="drive-obj whitespace-normal text-center text-lg px-16 py-6 shadow-inner col-span-2 md:col-span-1">
+    {name}
+    <div id="file" class="text-xs text-gray-500">
+        ({id})
+    </div>
+</div>
---
+++ src/components/RenderDrives.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Drive from './Drive.svelte'
+  export let promise
+</script>
+
+<div id="content-list" class="grid grid-cols-1 md:grid-cols-2">
+  {#await promise}
+    <div />
+  {:then promisee}
+    {#each promisee as item}
+      <Drive {...item} />
+    {/each}
+    {#if promisee == 0}
+      <div id="#loading" class="col-span-full">
+        <center>
+          <div class="lds-ripple">
+            <div />
+            <div />
+          </div>
+        </center>
+      </div>
+    {/if}
+  {:catch error}
+    <p>{error}</p>
+  {/await}
+</div>
