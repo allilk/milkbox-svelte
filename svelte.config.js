5ac03ecb977b205f10e54b3f8c63963ec620161d
allilk <milk@cream.glass>
2021-10-09 11:16:54 -0400

    netlify functionality

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
